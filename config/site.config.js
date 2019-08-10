9b4caec7ef36234aa5482d7d5a82973d71898e87
Daiane <adm.daianesilva@gmail.com>
2019-08-10 15:31:07 -0300

    Google Analytics

--- config/site.config.js
+++ config/site.config.js
@@ -12,7 +12,7 @@ const config = {
   site_name: 'Communitech',
 
   // Your website's description, used for favicon meta tags
-  site_description: 'Desenvolver Inovações em Tecnologia e Educação para a inclusão de residentesde bairros periféricos no mundo digital e redução de desigualdades',
+  site_description: 'A CommuniTech vem com uma proposta inovadora, visando dar capacitação de nível básico em informática para que os nossos jovens da comunidade tenham a oportunidade de ingressar no mercado de trabalho em um nível técnico, no intuito de diminuir a desigualdade social.',
 
   // Your website's URL, used for sitemap
   site_url: 'http://communitech.tk',
