27d06da05273c563e5c746d7ce6ec26bd9471b38
Daiane <adm.daianesilva@gmail.com>
2019-08-11 18:43:09 -0300

    close sidebar

--- src/javascripts/scripts.js
+++ src/javascripts/scripts.js
@@ -1,6 +1,7 @@
 const menuHamburguer = document.querySelector('#menu-hamburguer');
 const sideBar = document.querySelector('.sidebar');
 const blocked = document.querySelector('#blocked');
+const close = document.querySelector('#close');
 sideBar.classList.add('closed');
 
 function open() {
@@ -17,3 +18,4 @@ function closed() {
 
 menuHamburguer.onclick = open;
 blocked.onclick = closed;
+close.onclick = closed;
