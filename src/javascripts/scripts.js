863e11eeda30884ea4a14b1fa3204e821fa95a90
Daiane <adm.daianesilva@gmail.com>
2019-08-11 17:55:42 -0300

    sidebar mobile

--- src/javascripts/scripts.js
+++ src/javascripts/scripts.js
@@ -1,10 +1,19 @@
 const menuHamburguer = document.querySelector('#menu-hamburguer');
 const sideBar = document.querySelector('.sidebar');
+const blocked = document.querySelector('#blocked');
 sideBar.classList.add('closed');
 
 function open() {
   sideBar.classList.remove('closed');
   sideBar.classList.add('open');
+  blocked.classList.add('blocked');
+}
+
+function closed() {
+  sideBar.classList.remove('open');
+  sideBar.classList.add('closed');
+  blocked.classList.remove('blocked');
 }
 
 menuHamburguer.onclick = open;
+blocked.onclick = closed;
